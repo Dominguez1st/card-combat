81aa7f0cafdf3b9ae62573abfc3d33664b3696c4
Robert Dominguez <dominguez.1st@gmail.com>
2020-12-01 16:24:44 -0700

    CardCombatApplication.java documentation done.

--- app/src/main/java/edu/cnm/deepdive/cardcombat/CardCombatApplication.java
+++ app/src/main/java/edu/cnm/deepdive/cardcombat/CardCombatApplication.java
@@ -6,8 +6,14 @@ import edu.cnm.deepdive.cardcombat.service.CardCombatDatabase;
 import edu.cnm.deepdive.cardcombat.service.GoogleSignInService;
 import io.reactivex.schedulers.Schedulers;
 
+/**
+ * This class serves as the entry point for the ScaleScroller application.
+ */
 public class CardCombatApplication extends Application {
 
+  /**
+   * Initializes {@link CardCombatDatabase} and {@link GoogleSignInService}.
+   */
   @Override
   public void onCreate() {
     super.onCreate();
