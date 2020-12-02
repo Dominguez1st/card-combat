50210345086347e4ddd0fcfc7cd192b777bb4907
Robert Dominguez <dominguez.1st@gmail.com>
2020-12-02 15:56:51 -0700

    Finished the JavaDocs for the CardCombatDatabase.java.

--- app/src/main/java/edu/cnm/deepdive/cardcombat/service/CardCombatDatabase.java
+++ app/src/main/java/edu/cnm/deepdive/cardcombat/service/CardCombatDatabase.java
@@ -22,6 +22,11 @@ import edu.cnm.deepdive.cardcombat.service.CardCombatDatabase.Converters;
 import java.nio.ByteBuffer;
 import java.util.UUID;
 
+/**
+ * The CardCombat Database provides abstract methods that return the DAOs for each entity. It
+ * also provides methods to set the context and to return an instance of itself. It contains a
+ * public nested class: Converters.
+ */
 @Database(entities = {AttackCard.class, Deck.class, Game.class, UnitCard.class, User.class}, version = 1, exportSchema = true)
 @TypeConverters(value = {Converters.class, AttackType.class, Unit.class})
 public abstract class CardCombatDatabase extends RoomDatabase {
@@ -30,22 +35,57 @@ public abstract class CardCombatDatabase extends RoomDatabase {
 
   private static Application context;
 
+  /**
+   * Sets the application context.
+   *
+   * @param context The application context.
+   */
   public static void setContext(Application context) {
     CardCombatDatabase.context = context;
   }
 
+  /**
+   * Returns an instance of the singleton database.
+   *
+   * @return An instance of {@code ScaleScrollerDatabase}
+   */
   public static CardCombatDatabase getInstance() {
     return InstanceHolder.INSTANCE;
   }
 
+  /**
+   * Returns the AttackCardDao.
+   *
+   * @return The AttackCardDao.
+   */
   public abstract AttackCardDao getAttackCardDao();
 
+  /**
+   * Returns the DeckDao.
+   *
+   * @return The DeckDao.
+   */
   public abstract DeckDao getDeckDao();
 
+  /**
+   * Returns the GameDao.
+   *
+   * @return The GameDao.
+   */
   public abstract GameDao getGameDao();
 
+  /**
+   * Returns the UnitCardDao.
+   *
+   * @return The UnitCardDao.
+   */
   public abstract UnitCardDao getUnitCardDao();
 
+  /**
+   * Returns the UserDao.
+   *
+   * @return The UserDao.
+   */
   public abstract UserDao getUserDao();
 
   private static class InstanceHolder {
@@ -56,8 +96,18 @@ public abstract class CardCombatDatabase extends RoomDatabase {
 
   }
 
+  /**
+   * Provides type converters to translate Java objects into data that can be stored in the SQLite
+   * database.
+   */
   public static class Converters {
 
+    /**
+     * Converts a {@link UUID} object into bytes.
+     *
+     * @param value A {@code UUID} object.
+     * @return An byte
+     */
     @TypeConverter
     public static byte[] uuidToBytes(UUID value) {
       byte[] bytes = null;
@@ -70,6 +120,12 @@ public abstract class CardCombatDatabase extends RoomDatabase {
       return bytes;
     }
 
+    /**
+     * Converts a byte  into a {@link UUID} object.
+     *
+     * @param value A {@code UUID} object.
+     * @return An UUID
+     */
     @TypeConverter
     public static UUID bytesToUUID(byte[] value) {
       UUID uuid = null;
