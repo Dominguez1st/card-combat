6d6bf4717682607198df3ad0dd0028c03e395a02
Robert Dominguez <dominguez.1st@gmail.com>
2020-12-02 16:44:54 -0700

    Finished the JavaDocs for the MainViewModel.java.

--- app/src/main/java/edu/cnm/deepdive/cardcombat/viewmodel/MainViewModel.java
+++ app/src/main/java/edu/cnm/deepdive/cardcombat/viewmodel/MainViewModel.java
@@ -8,13 +8,21 @@ import edu.cnm.deepdive.cardcombat.model.entity.User;
 import edu.cnm.deepdive.cardcombat.service.GoogleSignInService;
 import edu.cnm.deepdive.cardcombat.service.UserRepository;
 
+/**
+ * Serves as the ViewModel for all fragments except the GameFragment.
+ */
 public class MainViewModel extends AndroidViewModel {
 
   private final MutableLiveData<User> currentUser;
   private final GoogleSignInService signInService;
   private final UserRepository userRepository;
 
-
+  /**
+   * The constructor initializes repositories, services, and other elements needed by the
+   * ViewModel.
+   *
+   * @param application The CardCombat application.
+   */
   public MainViewModel(
       @NonNull Application application) {
     super(application);
