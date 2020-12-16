e97b17d3bce209a356f31960be63433e10914161
Robert Dominguez <dominguez.1st@gmail.com>
2020-12-15 17:10:35 -0700

    Added some stuff.

--- app/src/main/java/edu/cnm/deepdive/cardcombat/model/type/Skill.java
+++ app/src/main/java/edu/cnm/deepdive/cardcombat/model/type/Skill.java
@@ -5,11 +5,11 @@ package edu.cnm.deepdive.cardcombat.model.type;
  */
 public enum Skill {
   HEAL(20),
-  DOUBLE_DAMAGE(10),
+  DOUBLE_DAMAGE(15),
   TARGET(10),
   ADD_CRITICAL(5),
-  ASSASSINATE(7),
-  SHIELD(5);
+  ASSASSINATE(10),
+  SHIELD(10);
 
   private final int cost;
 
